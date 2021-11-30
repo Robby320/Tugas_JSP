f16df4939ee22fc09d7babcb36b855b2de4ca5e9
Robby320 <95282653+Robby320@users.noreply.github.com>
2021-11-30 17:48:31 +0700

    Add files via upload

---
+++ pembayaran_kas.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 30, 2021 at 11:17 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pembayaran_kas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uang_kas`
+--
+
+CREATE TABLE `uang_kas` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `kelas` varchar(50) DEFAULT NULL,
+  `nominal` int(11) DEFAULT NULL,
+  `tanggal_bayar` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `uang_kas`
+--
+
+INSERT INTO `uang_kas` (`id`, `nama`, `kelas`, `nominal`, `tanggal_bayar`) VALUES
+(1, 'Robby Fernando', 'SI 5A', 50000, '2021-11-25'),
+(2, 'Uzumaki Naruto', 'SI 5A', 25000, '2021-11-30'),
+(3, 'Uchiha Sasuke', 'SI 5A', 21000, '2021-11-30');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `uang_kas`
+--
+ALTER TABLE `uang_kas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `uang_kas`
+--
+ALTER TABLE `uang_kas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ tambah_data.jsp
@@ -0,0 +1,19 @@
+<%@include file="koneksi.jsp"%>
+<%
+String submit = request.getParameter("button_submit");
+if (submit == null) {
+    response.sendRedirect("./");
+}
+String nama = request.getParameter("nama");
+String kelas = request.getParameter("kelas");
+String nominal = request.getParameter("nominal");
+String tanggal_bayar = request.getParameter("tanggal_bayar");
+String query = "INSERT INTO uang_kas(nama, kelas, nominal, tanggal_bayar) VALUES(?,?,?,?)";
+PreparedStatement ps = con.prepareStatement(query);
+ps.setString(1, nama);
+ps.setString(2, kelas);
+ps.setString(3, nominal);
+ps.setString(4, tanggal_bayar);
+ps.executeUpdate();
+response.sendRedirect("./");
+%>
